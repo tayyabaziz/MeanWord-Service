8c3c008b37366a5739ec7b3f6e3ae2a12609c72a
tayyabaziz <tayyabaziz5@gmail.com>
2019-08-07 16:35:19 +0500

    Invalid Parsing

--- app/services/word.service.js
+++ app/services/word.service.js
@@ -1,5 +1,6 @@
+const ObjectId = require('mongoose').Types.ObjectId
 const { WordModel } = require('../models/all.models')
-const { ResourceNotFoundError, DatabaseError } = require('../errors/errors')
+const { ResourceNotFoundError, DatabaseError, InvalidDataError } = require('../errors/errors')
 
 class WordService {
   constructor () {
@@ -27,20 +28,24 @@ class WordService {
 
   getWord (data) {
     return new Promise((resolve, reject) => {
-      this.Word
-        .findById(data.wordId)
-        .exec((err, data) => {
-          console.log(err)
-          if (err) {
-            reject(new DatabaseError(err.message, err.name))
-          } else {
-            if (data === undefined || data.length === 0) {
-              reject(new ResourceNotFoundError('Word'))
+      if (ObjectId.isValid(data.wordId)) {
+        this.Word
+          .findById(data.wordId)
+          .exec((err, data) => {
+            console.log(err)
+            if (err) {
+              reject(new DatabaseError(err.message, err.name))
             } else {
-              resolve(data)
+              if (data === undefined || data.length === 0) {
+                reject(new ResourceNotFoundError('Word'))
+              } else {
+                resolve(data)
+              }
             }
-          }
-        })
+          })
+      } else {
+        reject(new InvalidDataError('Word ID'))
+      }
     })
   }
 
@@ -64,19 +69,23 @@ class WordService {
 
   updateWord (data) {
     return new Promise((resolve, reject) => {
-      this.Word
-        .findByIdAndUpdate(data.wordId, data.wordData, { new: true, runValidators: true })
-        .exec((err, data) => {
-          if (err) {
-            reject(new DatabaseError(err.message, err.name))
-          } else {
-            if (data === undefined || data.length === 0) {
-              reject(new ResourceNotFoundError('Word'))
+      if (ObjectId.isValid(data.wordId)) {
+        this.Word
+          .findByIdAndUpdate(data.wordId, data.wordData, { new: true, runValidators: true })
+          .exec((err, data) => {
+            if (err) {
+              reject(new DatabaseError(err.message, err.name))
             } else {
-              resolve(data)
+              if (data === undefined || data.length === 0) {
+                reject(new ResourceNotFoundError('Word'))
+              } else {
+                resolve(data)
+              }
             }
-          }
-        })
+          })
+      } else {
+        reject(new InvalidDataError('Word ID'))
+      }
     })
   }
 }
